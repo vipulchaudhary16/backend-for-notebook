b32069744f4265dd1aee8d494e9776383e08a9ab
vipulchaudhary16 <vipulr6111@gmail.com>
2021-10-07 22:13:02 +0530

    express-validator added for unique User

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator')
 
 //create a User using POST "/api/auth/"
-router.post('/' , (req , res)=>{
-    // console.log(req.body)
-    const user = User(req.body)
-    user.save();
-    // res.send(req.body)
-    res.send("Authantication successful")
+router.post('/', [
+    //Setting validator for not allowing false log in and security
+    body('name', 'Name must have minimum 2 character').isLength({ min: 2 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must have minimum 5 character').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create(
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        }).then(user => res.json(user))
+        .catch(e => {
+            console.log(e)
+            res.json({ error: 'User already exists with this email' })
+        })
 })
 
+// res.send("Authantication successful")
 module.exports = router;
